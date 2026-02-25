0c643f5cabb43b2ba4ff6cb8e42d509bf4773f36
Siran-web <simranhaidar91@gmail.com>
2026-02-25 16:25:42 +0530

    feat : Add authentication

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hero from "./components/mainPage/Hero";
 import Booking from "./components/BookingPage/BookingHero";
 import NavBar from "./components/Navbar";
+import LoginPage from "./components/auth/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar/>
       <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/booking" element={<Booking />} />
+        {/* <Route path="/" element={<Hero />} />
+        <Route path="/booking" element={<Booking />} /> */}
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
